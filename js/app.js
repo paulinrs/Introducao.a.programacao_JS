3239f950327eb1e91a1f93ebc6726a596d7f3d4d
ruby --version <paulinn.rs@gmail.com>
2023-02-05 09:49:09 -0300

    variaveis de tipos de dados

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Olá, Javascript!')
+
+var userName = 'Paulinho'
+
+document.getElementById('user_name').innerHTML = userName
+
+//var nome = 'Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
