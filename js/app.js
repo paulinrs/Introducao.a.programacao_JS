3319f16e0858a6a21353257991615b9412fe1175
ruby --version <paulinn.rs@gmail.com>
2023-02-05 10:39:45 -0300

    Operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var userName = 'Paulinho'
 
 document.getElementById('user_name').innerHTML = userName
 
+//variáveis//
+
 //var nome = 'Yoda'
 //var idade = 100
 //var jedi = true
@@ -11,3 +13,19 @@ document.getElementById('user_name').innerHTML = userName
 //console.log(typeof nome)
 //console.log(typeof idade)
 //console.log(typeof jedi)
+
+//Operadores matemáticos//
+
+// (+) para somar
+// (-) para subtrair
+// (*) para multiplicar
+// (/) para dividir
+
+var n1 = 11
+var n2 = 11
+
+var total = n1 + n2
+console.log(total)
+
+
+
