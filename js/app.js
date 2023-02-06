d792ac27b48fb67c2e27140691bde6aeb46700d9
ruby --version <paulinn.rs@gmail.com>
2023-02-06 17:26:49 -0300

    Operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -21,11 +21,70 @@ document.getElementById('user_name').innerHTML = userName
 // (*) para multiplicar
 // (/) para dividir
 
-var n1 = 11
-var n2 = 11
+// var n1 = 11
+// var n2 = 11
 
-var total = n1 + n2
-console.log(total)
+// console.log(typeof n1)
+// console.log(typeof n2)
+
+// var total = n1 + n2
+// console.log(total)
+
+//Operadores de comparação//
+
+// Igual (==)
+// Retorna verdadeiro se os valores comparados forem iguais.
+// 1=='1' //true
+
+// Não igual (!=)
+// Retorna verdadeiro se os valores comparados não forem iguais. Esse operador também pode ser chamado de diferente de.
+// 4!=1 //true
+
+// Igual estrito (===)
+// Esse operador é mais severo, só retorna verdadeiro se o valor e o tipo comparados forem iguais.
+// 3==='3' //false
+// 3===3 //true
+
+// Não igual estrito (!==)
+// Não se engane, esse operador vai retornar verdadeiro se os valores e ou os tipos forem diferentes
+// 3!=='3' //true
+// 3!==3 //false
+// 3!==4 //true
+
+// Maior que (>)
+// Compara se o operador da esquerda é maior que o da direita. Caso seja retorna verdadeiro
+// 1>2 //false
+// 5>3 //true
+// 4>'1' //true
+
+// Maior ou igual que (>=)
+// Compara se o operador da esquerda é maior ou igual ao da direita. Caso seja retorna verdadeiro
+// 1>=2 //false
+// 5>=3 //true
+// 4>='1' //true
+// 2>=2 // true
+
+// Menor que (<)
+// Compara se o operador da esquerda é menor que o da direita. Caso seja retorna verdadeiro
+
+// 1<2 //true
+// 5<3 //false
+// 4<'1' //false    
+
+// Menor ou igual que (<=)
+// Compara se o operador da esquerda é menor ou igual ao da direita. Caso seja retorna verdadeiro
+
+// 1<=2 //true
+// 5<=3 //false
+// 4<='1' //false
+// 2<=2 // true
+
+//EXEMPLO//
+// var v1 = 5
+// var v2 = 5
+
+// var resultado = v1 === v2
+// console.log(resultado)
 
 
 
