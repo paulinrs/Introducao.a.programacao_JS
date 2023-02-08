b5642f7c9ddaf59f4cf7d6fca69ffd78c95e53f5
ruby --version <paulinn.rs@gmail.com>
2023-02-08 09:34:14 -0300

    funções

--- js/app.js
+++ js/app.js
@@ -86,5 +86,23 @@ document.getElementById('user_name').innerHTML = userName
 // var resultado = v1 === v2
 // console.log(resultado)
 
+//Funções//
 
+// function soma(n1, n2){
+//     console.log(n1 + n2)
+// }
+
+//soma(5, 100)
+
+// function boasVindas(nome) {
+//     alert(nome + ', seja bem-vindo(a)')
+// }
+// boasVindas('Paulo Roberto')
+
+// function soma(n1, n2){
+//     return n1 + n2
+// }
+
+// var resultado = soma(5,5)
+// console.log(resultado)
 
