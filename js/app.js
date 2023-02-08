be818217bc8b1ec3bef596c46730725dd25045b2
ruby --version <paulinn.rs@gmail.com>
2023-02-08 16:31:03 -0300

    Controle de fluxos com BDD

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var userName = 'Paulinho'
 
 document.getElementById('user_name').innerHTML = userName
 
-//variáveis//
+//*Variáveis*//
 
 //var nome = 'Yoda'
 //var idade = 100
@@ -14,13 +14,14 @@ document.getElementById('user_name').innerHTML = userName
 //console.log(typeof idade)
 //console.log(typeof jedi)
 
-//Operadores matemáticos//
+//*Operadores matemáticos*//
 
 // (+) para somar
 // (-) para subtrair
 // (*) para multiplicar
 // (/) para dividir
 
+//->EXEMPLO
 // var n1 = 11
 // var n2 = 11
 
@@ -28,65 +29,73 @@ document.getElementById('user_name').innerHTML = userName
 // console.log(typeof n2)
 
 // var total = n1 + n2
-// console.log(total)
+// console.log(total)<-
 
-//Operadores de comparação//
+//*Operadores de comparação*//
 
 // Igual (==)
 // Retorna verdadeiro se os valores comparados forem iguais.
 // 1=='1' //true
 
 // Não igual (!=)
-// Retorna verdadeiro se os valores comparados não forem iguais. Esse operador também pode ser chamado de diferente de.
+// Retorna verdadeiro se os valores comparados não forem iguais. ->
+//Esse operador também pode ser chamado de diferente de.
 // 4!=1 //true
 
 // Igual estrito (===)
-// Esse operador é mais severo, só retorna verdadeiro se o valor e o tipo comparados forem iguais.
+// Esse operador é mais severo, só retorna verdadeiro ->
+//se o valor e o tipo comparados forem iguais.
 // 3==='3' //false
 // 3===3 //true
 
 // Não igual estrito (!==)
-// Não se engane, esse operador vai retornar verdadeiro se os valores e ou os tipos forem diferentes
+// Não se engane, esse operador vai retornar verdadeiro ->
+//se os valores e ou os tipos forem diferentes
 // 3!=='3' //true
 // 3!==3 //false
 // 3!==4 //true
 
 // Maior que (>)
-// Compara se o operador da esquerda é maior que o da direita. Caso seja retorna verdadeiro
+// Compara se o operador da esquerda é maior que o da direita. ->
+//Caso seja retorna verdadeiro
 // 1>2 //false
 // 5>3 //true
 // 4>'1' //true
 
 // Maior ou igual que (>=)
-// Compara se o operador da esquerda é maior ou igual ao da direita. Caso seja retorna verdadeiro
+// Compara se o operador da esquerda é maior ou igual ao da direita. ->
+//Caso seja retorna verdadeiro
+
 // 1>=2 //false
 // 5>=3 //true
 // 4>='1' //true
 // 2>=2 // true
 
 // Menor que (<)
-// Compara se o operador da esquerda é menor que o da direita. Caso seja retorna verdadeiro
+// Compara se o operador da esquerda é menor que o da direita. ->
+//Caso seja retorna verdadeiro
 
 // 1<2 //true
 // 5<3 //false
 // 4<'1' //false    
 
 // Menor ou igual que (<=)
-// Compara se o operador da esquerda é menor ou igual ao da direita. Caso seja retorna verdadeiro
+// Compara se o operador da esquerda é menor ou igual ao da direita. ->
+//Caso seja retorna verdadeiro
 
 // 1<=2 //true
 // 5<=3 //false
 // 4<='1' //false
 // 2<=2 // true
 
-//EXEMPLO//
+//->EXEMPLO
 // var v1 = 5
 // var v2 = 5
 
 // var resultado = v1 === v2
-// console.log(resultado)
+// console.log(resultado) <-
 
-//Funções//
+//*Funções*//
 
 // function soma(n1, n2){
 //     console.log(n1 + n2)
@@ -106,3 +115,79 @@ document.getElementById('user_name').innerHTML = userName
 // var resultado = soma(5,5)
 // console.log(resultado)
 
+//*Controle de fluxos com BDD*//
+
+//(Historia de usuario)
+
+//Sendo um cliente correntista do banco
+//Posso sacar dinheiro em caixas eletrônicos 
+//Para poder comprar em lugares que não aceitam o cartão de débito ou crédito
+
+//(BDD)
+
+//Cenario 1: Saque com Sucesso
+//Dado que meu saldo é de 1000 reais 
+//Quando faço um saque de 500 reais 
+//Então o valor do saque deve ser deduzido do meu saldo
+
+//Cenario 2: Saque com valor superior ao saldo
+//Dado que meu saldo é de 600 reais
+//quando faço um saque de 601 reais
+//Então não deve deduzir do meu saldo
+//E deve mostrar uma mensagem de alerta informando que o valor é superior ao saldo
+
+//Cenario 3: Saque com valor máximo
+//Dado que meu saldo é de 1000 reais
+//E o valor máximo por operação é de 700 reais
+//Quando faço um saque com valor e 701 reais
+//Então não deve deduzir do meu saldo
+//E deve mostrar uma mensagem de alerta informando que o valor é superior ao máximo permitido por operação
+
+//EXEMPLOS->
+
+//->Cenario 1*
+
+// var saldo = 1000
+
+// function saque(valor) {
+//     saldo = saldo - valor
+// }
+
+// saque(500)
+// console.log(saldo)<-
+
+//->Cenario 2*
+
+// var saldo = 600
+
+// function saque(valor) {
+
+//     if(valor > saldo){
+//         console.log('Valor do saque superior ao saldo')
+//     } else{
+//         saldo = saldo - valor
+//     }
+
+// }
+
+// saque(601)
+// console.log(saldo)
+
+//->Cenario 3*
+
+// var saldo = 1000
+
+// function saque(valor) {
+
+//     if(valor > saldo){
+//         console.log('Valor do saque superior ao saldo')
+//     } else if (valor > 700) {
+//         console.log('Valor de saque superior ao maximo permitido por operação')
+//     } else{
+//         saldo = saldo - valor
+//     }
+
+// }
+
+// saque(701)
+// console.log(saldo)
