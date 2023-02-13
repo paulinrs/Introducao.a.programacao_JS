8d46bf221bfdc1ed0a56044822a927cea524beec
ruby --version <paulinn.rs@gmail.com>
2023-02-12 21:22:55 -0300

    Implementando funções

--- js/app.js
+++ js/app.js
@@ -47,5 +47,10 @@ const App = new Vue({
         title: 'Star Wars Lego',
         userName: 'Paulinho',
         characters: LIST
-    }
+    },
+    methods:{
+        like(userName){
+            alert(`O personagem ${userName} recebeu um like!`)
+        }
+    }    
 })
